ee298e71b5d7b73fabdaf40fccab4e7b921bc7d2
HOP3N <pankoff97@gmail.com>
2023-01-31 17:33:15 +0200

    indx+ ++ sm upd

--- src/index.js
+++ src/index.js
@@ -11,23 +11,18 @@ const DEBOUNCE_DELAY = 300;
 input.addEventListener(
   'input',
   debounce(e => {
-      const trimmedValue = input.value.trim();
-         cleanHtml();   
-    if (trimmedValue !== '') {
-        fetchCountries(trimmedValue).then(foundData => {      
-
-        if (foundData.length > 10) {
+    const onTrimValue = input.value.trim();
+    cleanHTML();
+    if (onTrimValue !== '') {
+      fetchCountries(onTrimValue).then(onDataFound => {
+        if (onDataFound.length > 10) {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
-        } else if (foundData.length === 0) {
+        } else if (onDataFound.length === 0) {
           Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (foundData.length >= 2 && foundData.length <= 10) {
-         
-          renderCountryList(foundData);
-        } else if (foundData.length === 1) {
-    
-          renderOneCountry(foundData);
+        } else if (onDataFound.length >= 2 && onDataFound.length <= 10) {
+          renderCountryList(onDataFound);
         }
       });
     }
@@ -38,34 +33,32 @@ function renderCountryList(countries) {
   const markup = countries
     .map(country => {
       return `<li>
-      <img src="${country.flags.svg}" alt="Flag of ${
-        country.name.official
-      }" width="30" hight="20">
-         <b>${country.name.official}</p>
-                </li>`;
+    <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="28" hight="18">
+    <p>${country.name.official}</p>
+    </li>`;
     })
     .join('');
   countryList.innerHTML = markup;
 }
 
-function renderOneCountry(countries) {
-      const markup = countries
-        .map(country => {
-          return `<li>
-      <img src="${country.flags.svg}" alt="Flag of ${
-            country.name.official
-          }" width="30" hight="20">
-         <b>${country.name.official}</b></p>
-            <p><b>Capital</b>: ${country.capital}</p>
-            <p><b>Population</b>: ${country.population}</p>
-            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
-                </li>`;
-        })
-        .join('');
-      countryList.innerHTML = markup;
+function renderCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+    <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="28" hight="18">
+    <p>${country.name.official}</p>
+    <p>Capital: ${country.capital}</p>
+    <p>Population: ${country.population}</p>
+    <p>Language: ${Object.values(country.languages)}</p>
+    </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
 }
 
-function cleanHtml() {
+function cleanHTML() {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
 }
